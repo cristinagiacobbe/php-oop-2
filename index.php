ca4bf1a9327efbe771fe35d05120282359ab8794
cristinagiacobbe <crigiak@gmail.com>
2024-03-21 00:06:41 +0100

    Fix class category in html file

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ class Products
     /**
      * Generic class of product (parent class)
      */
-    public function __construct(public $name, public Categories $categories, public $image, public $price)
+    public function __construct(public $name, public Categories $category, public $image, public $price)
     {
         $this->name = $name;
-        $this->categories = $categories;
+        $this->category = $category;
         $this->image = $image;
         $this->price = $price;
     }
@@ -34,9 +34,9 @@ class Food extends Products
     /**
      * Specific class of product (child class)
      */
-    public function __construct($name, Categories $categories, $image, $price, public $ingredients)
+    public function __construct($name, Categories $category, $image, $price, public $ingredients)
     {
-        parent::__construct($name, $categories, $image, $price);
+        parent::__construct($name, $category, $image, $price);
         $this->ingredients = $ingredients;
     }
 }
@@ -46,9 +46,9 @@ class Toys extends Products
     /**
      * Specific class of product (child class)
      */
-    public function __construct($name, Categories $categories, $image, $price, public $material)
+    public function __construct($name, Categories $category, $image, $price, public $material)
     {
-        parent::__construct($name, $categories, $image, $price);
+        parent::__construct($name, $category, $image, $price);
         $this->material = $material;
     }
 }
@@ -58,9 +58,9 @@ class Sits extends Products
     /**
      * Specific class of product (child class)
      */
-    public function __construct($name, Categories $categories, $image, $price, public $material, public $measure)
+    public function __construct($name, Categories $category, $image, $price, public $material, public $measure)
     {
-        parent::__construct($name, $categories, $image, $price);
+        parent::__construct($name, $category, $image, $price);
         $this->material = $material;
         $this->measure = $measure;
     }
@@ -98,35 +98,41 @@ foreach ($products as $product) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Animal e-commerce shop</title>
-    <link rel="stylesheet" href="style.css">
+
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <h1>Animal product</h1>
+
     <div class="container text-center">
+        <h1>Animal products</h1>
         <div class="row">
-            <div class="col-2 col-sm-1">
-                <div class="card">
-                    <ul class="list-unstyled">
-                        <?php foreach ($products as $product) : ?>
-                            <li><?php foreach ($product as $typeProduct) : ?>
-                                    <p><?php echo $typeProduct->name ?></p>
+            <div class="col-4">
 
-                                    <p><?php echo $typeProduct->price ?></p>
-                                    <img src="<?php echo $typeProduct->image ?>" alt="">
-                                    <?php foreach ($typeProduct as $category) : ?>
-                                        <i class="<?php echo $category->categories ?>"></i>
-                                    <?php endforeach; ?>
 
-                                <?php endforeach; ?>
-                            </li>
+                <?php foreach ($products as $product) : ?>
+                    <?php foreach ($product as $typeProduct) : ?>
+                        <div class="card">
+                            <h2><?php echo $typeProduct->name ?></h2>
 
+                            <p><?php echo $typeProduct->price ?></p>
+                            <img src="<?php echo $typeProduct->image ?>" alt="">
+
+                            <?php
+                            $category = $typeProduct->category;
+                            ?>
+                            <div>
+                                <i class="<?php echo $category->icon ?>"></i>
+                            </div>
                         <?php endforeach; ?>
-                    </ul>
-                </div>
+
+
+                    <?php endforeach; ?>
+                        </div>
+
+
             </div>
         </div>
     </div>
