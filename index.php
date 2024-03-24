1c074fd22c575308d110ce1e0927bb38f4cf2684
cristinagiacobbe <crigiak@gmail.com>
2024-03-24 22:32:49 +0100

    Fix layout

--- index.php
+++ index.php
@@ -31,32 +31,34 @@ require_once __DIR__ . "/Database/db.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Animal e-commerce shop</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
     <div class="container">
-        <h1 class="mt-5">Animal products</h1>
+        <h1>Animal products</h1>
 
-        <div class="row row-cols-1 row-cols-sm-2 g-3 ">
+        <div class="row row-cols-2 g-3 ">
 
             <?php foreach ($products as $product) : ?>
                 <?php foreach ($product as $typeProduct) : ?>
                     <div class="col">
-                        <div class="card">
-                            <h2><?php echo $typeProduct->name ?></h2>
-                            <img src="<?php echo $typeProduct->image ?>" alt="Product image">
-                            <?php
-                            $category = $typeProduct->category;
-                            ?>
-                            <div>
-                                <i class="<?php echo $category->icon ?>"></i>
+                        <div class="card" style="height: 300px" ;>
+                            <img class="card-img-top rounded mx-auto d-block" src=" <?= $typeProduct->image ?>" alt="Product image">
+                            <div class="card-body">
+                                <h3><?php echo $typeProduct->name ?></h3>
+                                <?php
+                                $category = $typeProduct->category;
+                                ?>
+                                <div>
+                                    <i class="<?= $category->icon ?>"></i>
+                                </div>
+                                <p><?= $typeProduct->price ?></p>
                             </div>
-                            <p><?php echo $typeProduct->price ?></p>
                         </div>
                     </div>
                 <?php endforeach; ?>
